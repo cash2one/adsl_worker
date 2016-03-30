886c817c42ca5fe65fa7babbf35e163033e01b3b
wgy-ts <wgy-ts@163.com>
2016-03-30 14:58:02 +0800

    coding

--- src/index.py
+++ src/index.py
@@ -25,11 +25,11 @@ def dail(ip_idc):
         data = urllib.urlencode({'dail': True})
         ret = urllib.urlopen(url, data).read()
         tm = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
-        print 'dailing ' + ip_idc
-        return str(tm) + ' ' + ip_idc + " dailed!"
+        # print 'dailing ' + ip_idc
+        print str(tm) + ' ' + ip_idc + " dailed!"
     except Exception:
-        return False
-
+        # return False
+        pass
 
 def main():
     adsl = Adsl(adsl_config['host'], adsl_config['port'])
@@ -37,31 +37,34 @@ def main():
     while True:
         lines = adsl.getlines()
         ips = []
+        threads = []
 
         for line in lines:
             if adsl.getstatusbyline(line) == 'dailing':
                 # print line
                 ip_idc = adsl.getidcbyline(line)
-                # t = MyThread(dail, (ip_idc,), name=line)
-                # threads.append(t)
-                ips.append(ip_idc)
+                t = MyThread(dail, (ip_idc,), name=line)
+                t.start()
+                threads.append(t)
+                print 'start dail ' + line
+                # ips.append(ip_idc)
 
-        if len(ips) == 0:
+        if len(threads) == 0:
             time.sleep(1)
         else:
-            threads = []
-            for i in range(len(ips)):
-                t = threading.Thread(target=dail, args=(ips[i],))
-                threads.append(t)
-            print 'started!'
+            for t in threads:
+                t.join()
+                # t = threading.Thread(target=dail, args=(ips[i],))
+                # threads.append(t)
+            print 'join!'
 
-            for i in range(len(ips)):
-                threads[i].start()
+            # for i in range(len(ips)):
+            #     threads[i].start()
 
-            for i in range(len(ips)):
-                print 'before join'
-                threads[i].join()
-                print 'end join'
+            # for i in range(len(ips)):
+            #     print 'before join'
+            #     threads[i].join()
+            #     print 'end join'
                 # print threads[i].getResult()
             print 'end!'
 
